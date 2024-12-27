4389f605a02dbea555f79a964292608831138195
CristianCarrella <72307833+CristianCarrella@users.noreply.github.com>
2024-12-27 10:05:35 +0100

    Changed optimizer type

--- hyper_params.py
+++ hyper_params.py
@@ -1,9 +1,9 @@
 from torch.nn import CrossEntropyLoss
-from torch.optim import SGD
+from torch.optim import SGD, Optimizer
 
 
 class HyperParams:
-    def __init__(self, epochs: int, error_function: CrossEntropyLoss, optimizer: SGD, batch_size: int):
+    def __init__(self, epochs: int, error_function: CrossEntropyLoss, optimizer: Optimizer, batch_size: int):
         self.batch_size = batch_size
         self.error_function = error_function
         self.epochs = epochs
